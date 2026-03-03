83fc7219774c9a3af27aa26165ef171d82b59cc3
devil <6085devilbots@gmail.com>
2026-03-03 17:42:44 -0500

    Fixed AutoLaunchCommand Class

---
+++ src/main/java/frc/robot/subsystems/AutoLaunchCommand.java
@@ -0,0 +1,34 @@
+package frc.robot.subsystems;
+
+
+import com.pathplanner.lib.events.Event;
+import com.pathplanner.lib.path.PathPlannerPath;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.util.PathPlannerLogging;
+import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+
+import frc.robot.subsystems.Launcher;
+import frc.robot.Constants.DriveConstants;
+
+import com.pathplanner.lib.events.Event;
+
+import java.util.HashMap;
+
+public class AutoLaunchCommand extends InstantCommand {
+
+    public AutoLaunchCommand() {
+        super(() -> {
+            
+        Launcher.m_launcherClosedLoopController12.setSetpoint(DriveConstants.launcherInSpeed, SparkMax.ControlType.kVelocity);
+        Launcher.m_launcherClosedLoopController13.setSetpoint(DriveConstants.launcherInSpeed, SparkMax.ControlType.kVelocity);
+
+        });
+    }
+}
+
+
+
+
+
